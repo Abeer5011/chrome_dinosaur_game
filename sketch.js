c554fce74aa2b739960d0bab147802ba214a6905
Abeer5011 <92170094+Abeer5011@users.noreply.github.com>
2023-01-13 13:43:21 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -81,7 +81,7 @@ function gameOn() {
 function gameOver() {
   noLoop()
   text("Game over", width / 2, height / 6)
-  text("Ur Level is:", width / 2, height / 4)
+  text("Your Level is:", width / 2, height / 4)
   text(parseInt(Bird.levelUp), width / 2, height / 3)
   button.show()
 }
